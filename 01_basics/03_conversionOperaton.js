b89805df7b4eb93a2b31a3ad24ac4ead2d5834f2
Dev <113161893+debadattabehera169@users.noreply.github.com>
2024-03-08 20:53:37 +0000

    increment in datatypes

--- 01_basics/03_conversionOperaton.js
+++ 01_basics/03_conversionOperaton.js
@@ -1,26 +1,50 @@
-let score = "33abac"    //try with null undefiend true false 
+// let score = "33abac"    //try with null undefiend true false 
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
+// let valueInNumber = Number(score)
 
-//"33" = 33
-//"33abc" = NaN
-//true => 1
-//false => 0
+// //"33" = 33
+// //"33abc" = NaN
+// //true => 1
+// //false => 0
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
 
-let isLoggedIn =1
-let booleanIsLogedIn = Boolean(isLoggedIn)
-console.log (booleanIsLogedIn)
+// let isLoggedIn =1
+// let booleanIsLogedIn = Boolean(isLoggedIn)
+// console.log (booleanIsLogedIn)
 
-//"" => false
-// "Dev" => true
+// //"" => false
+// // "Dev" => true
 
-let someNumber = 33
-let stringNumber = String (someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String (someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+//***************************  operations************************/
+
+let value = 3
+let negValue = -value
+
+let str1 = "hello "
+let str2 = "Dev"
+let str3 = str1 + str2
+console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+
+console.log(+true);
+console.log(+"");
+
+let num1 =  num2 = num3 = 4
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
