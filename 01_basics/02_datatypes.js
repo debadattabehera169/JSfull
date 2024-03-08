245e0d1f24eab1d16417bff5a04544aa842805bc
Dev <113161893+debadattabehera169@users.noreply.github.com>
2024-03-08 19:42:18 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";  //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs not browser so its show us error bcz in browser js engine is priorly present
+
+console.log(3+3) //code readability should be high
+console.log("Dev")  //two documentation tc39 and mdn
+
+let name = "Dev"
+let age = 20
+let isLoggedIn = false
+
+//number => 2 to power 53 (range)
+//bigint
+//string => '' or ""
+//boolean => true/false
+//null (stand alone value)
+//undefined =>
+//symbol => unique
+//object 
+
+console.log(typeof undefined)
+console.log(typeof null)
