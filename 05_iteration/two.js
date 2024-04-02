f0aacffed83487e0e8a3bbaeeabaea9bf659334f
Dev <113161893+debadattabehera169@users.noreply.github.com>
2024-04-02 05:29:58 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,21 @@
+// while and do while loop
+
+let index =0
+while (index <=10) {
+    //console.log(`Value of index is  ${index}`);
+    index+=2
+}
+
+let myarray =["flash","batman","superman"]
+let arr =0
+while (arr <myarray.length) {
+    //console.log(`Value is ${myarray[arr]}`);
+    arr++
+}
+
+//do while loop
+let score = 1       //initialization
+do {
+    console.log(`Score is ${score}`);
+    score++         //updation
+} while (score <=10);       //condition
